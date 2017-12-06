b7ccbd20d9c31da19779a3bb7a963eca858b6373
Valerie Eskens <valerie.eskens@kbc.be>
2017-12-06 09:38:48 +0100

    2017 day 6

---
+++ 2017/day-06.js
@@ -0,0 +1,32 @@
+const input = [ 2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14]
+let scenarios = []
+
+function spreadIt() {
+    let max = Math.max(...input);
+    let maxIndex = input.indexOf(max)
+    input[maxIndex] = 0
+    while(max > 0) {
+        maxIndex++
+        if(maxIndex >= input.length) {
+            maxIndex -= input.length
+        }
+        input[maxIndex]++
+        max--
+    }
+    scenarios.push(input.join(''))
+}
+
+spreadIt()
+let lastIndex = scenarios.length-1
+let matchedIndex = scenarios.indexOf(scenarios[lastIndex])
+while(matchedIndex === lastIndex) {
+    spreadIt()
+    console.log(input)
+    lastIndex = scenarios.length-1
+    matchedIndex = scenarios.indexOf(scenarios[lastIndex])
+}
+
+console.log("steps: ", scenarios.length)
+console.log("matchedIndex: ", matchedIndex)
+console.log("lastIndex: ", lastIndex)
+console.log("difference", lastIndex-matchedIndex)
